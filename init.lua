33dad53324b40c06f510475d0acb4b1564fbb393
Badsheep <46049911+Badsheep@users.noreply.github.com>
2023-02-14 14:20:45 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,16 +1,20 @@
--- created by Badsheep
--- february 2023
+--created by Badsheep, february 2023
 
--- I'm a beginner in those kinf of stuffs so please don't judge me bad
--- The goal is to exchange Midi Datas between a Stresm Deck Mini and MAX (Cycling 74)
--- I uses Midi Control Change (n°2 and n°4)
--- 2 functions :
--- Activate the audio inputs of the soundcard to be able to hear a vinyl turntable connected on it
--- Activate a compressor on the audio output.
+--A Hammerspoon configuration
 
--- inspirations :
--- https://github.com/peterhajas/dotfiles/tree/master/hammerspoon/.hammerspoon
--- https://gist.github.com/llimllib/137c587eba2ce9b0eff2c9af4d0a5e29
+--I'm a beginner in those kind of stuffs so please don't judge me bad :) 
+--The goal is to exchange Midi Datas between a Stresm Deck Mini and MAX (Cycling 74) 
+--It uses Midi Control Change (n°2 and n°4)
+
+--2 functions in my case : 
+--Activate the audio inputs of the soundcard to be able to hear a vinyl turntable connected on it 
+--Activate a compressor on the audio output.
+
+--inspirations : 
+--https://github.com/peterhajas/dotfiles/tree/master/hammerspoon/.hammerspoon 
+--https://gist.github.com/llimllib/137c587eba2ce9b0eff2c9af4d0a5e29
+
+--Thanks to them !
 
 
 require "midi"
